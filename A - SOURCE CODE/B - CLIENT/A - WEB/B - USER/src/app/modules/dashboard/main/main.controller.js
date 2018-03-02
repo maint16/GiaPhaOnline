f53d986e8122bbf72761b0e4e2e87bd9a8d62556
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-03-02 17:25:07 +0800

    - Added categories list.

--- A - SOURCE CODE/B - CLIENT/A - WEB/B - USER/src/app/modules/dashboard/main/main.controller.js
+++ A - SOURCE CODE/B - CLIENT/A - WEB/B - USER/src/app/modules/dashboard/main/main.controller.js
@@ -1,19 +1,21 @@
 /*
 * Module exports.
 * */
-module.exports = function(ngModule){
-    ngModule.controller('mainDashboardController', function($scope, toastr,
-                                                            $timeout, $state,
-                                                            details,
-                                                            appSettings,
-                                                            urlStates,
-                                                            categoryService, postCategorizationService){
+module.exports = function (ngModule) {
+    ngModule.controller('mainDashboardController', function ($scope, toastr, $ngConfirm, $translate,
+                                                             $timeout, $state,
+                                                             appSettings, urlStates,
+                                                             profile,
+                                                             categoryService, postCategorizationService, followCategoryService) {
 
         //#region Properties
 
+        // Resolver reflection.
+        $scope.profile = profile;
+
         // Search result caching.
         $scope.result = {
-            getCategories:{
+            getCategories: {
                 records: [],
                 total: 0
             }
@@ -21,13 +23,14 @@ module.exports = function(ngModule){
 
         // Temporary storage for client caching.
         $scope.buffer = {
-            categoryPostsCounter: {}
+            categoryPostsCounter: {},
+            followingCategory: {}
         };
 
         // Search condition.
         $scope.condition = {
-            getCategories:{
-                pagination:{
+            getCategories: {
+                pagination: {
                     page: 1,
                     records: appSettings.pagination.default
                 }
@@ -41,25 +44,39 @@ module.exports = function(ngModule){
         /*
         * Called when a category is selected.
         * */
-        $scope.clickCategory = function(category){
+        $scope.clickCategory = function (category) {
             $state.go(urlStates.category.postListing.name, {categoryId: category.id});
+        };
+
+        /*
+        * Function for searching categories (start searching from the beginning)
+        * */
+        $scope.fnSearchCategories = function($event){
+            // Event is valid. Prevent its default behaviour.
+            if ($event)
+                $event.preventDefault();
+
+            // Reset pagination to page 1.
+            $scope.condition.getCategories.pagination.page = 1;
+
+            // Search for categories.
+            $scope.fnGetCategories();
         };
 
         /*
         * Get categories can concatenate the search result list,
         * */
-        $scope.fnGetCategories = function($event){
+        $scope.fnGetCategories = function () {
 
-            // Event is valid. Prevent its default behaviour.
-            if ($event)
-                $event.preventDefault();
-
-            // Reset page to 1.
-            $scope.condition.getCategories.pagination.page = 1;
+            // Clear buffer.
+            $scope.buffer.followingCategory = {};
+            $scope.buffer.categoryPostsCounter = {};
+            $scope.result.records = [];
+            $scope.result.total = 0;
 
             // Do searching.
             categoryService.getCategories($scope.condition.getCategories)
-                .then(function(getCategoriesResponse){
+                .then(function (getCategoriesResponse) {
                     var getCategoriesResult = getCategoriesResponse.data;
                     if (!getCategoriesResult) {
                         // Reset search result.
@@ -67,35 +84,36 @@ module.exports = function(ngModule){
                         throw 'Cannot get categories list';
                     }
 
-                    $scope.result.getCategories = getCategoriesResult;
                     return getCategoriesResult;
                 })
-                .then(function(getCategoriesResult){
+                .then(function (getCategoriesResult) {
 
                     var categories = getCategoriesResult.records;
                     if (!categories || categories.length < 1)
                         throw 'No category has been found';
 
                     // Get all categories posts.
-                    var getPostCategorizationPromises = [];
+                    var pPromises = [];
 
                     // Clear buffer.
                     $scope.buffer.categoryPostsCounter = {};
 
-                    angular.forEach(categories, function(category, iterator){
+                    angular.forEach(categories, function (category, iterator) {
+
+                        //#region Get category posts counter
 
                         // Build condition for getting post categorizations.
                         var getPostCategorizationCondition = {
                             categoryId: category.id,
-                            pagination:{
+                            pagination: {
                                 page: 1,
                                 records: 1
                             }
                         };
 
-                        // Build promise.
-                        var getPostCategorizationPromise = postCategorizationService.getPostCategorizations(getPostCategorizationCondition)
-                            .then(function(getPostCategorizationResponse){
+                        // Get category post counter promise.
+                        var pGetCategoryPostCounterPromise = postCategorizationService.getPostCategorizations(getPostCategorizationCondition)
+                            .then(function (getPostCategorizationResponse) {
 
                                 // Get api returned result.
                                 var getPostCategorizationResult = getPostCategorizationResponse.data;
@@ -115,23 +133,149 @@ module.exports = function(ngModule){
                             });
 
                         // Add promise to queue.
-                        getPostCategorizationPromises.push(getPostCategorizationPromise);
+                        pPromises.push(pGetCategoryPostCounterPromise);
+
+                        //#endregion
+
+                        //#region Get category following status
+
+                        // If user is authenticated. Search for his/her following category.
+                        if (profile) {
+                            // Build condition for getting category following status
+                            var getCategoryFollowingStatus = {
+                                categoryId: category.id,
+                                pagination: {
+                                    page: 1,
+                                    records: 1
+                                }
+                            };
+
+                            // Build get category following status promise.
+                            var pGetCategoryFollowingStatusPromise = followCategoryService.getFollowingCategories(getCategoryFollowingStatus)
+                                .then(function (getCategoryFollowingStatusResponse) {
+
+                                    // Get following status result.
+                                    var getCategoryFollowingStatusResult = getCategoryFollowingStatusResponse.data;
+                                    if (getCategoryFollowingStatusResult == null)
+                                        return;
+
+                                    // No record is found.
+                                    var records = getCategoryFollowingStatusResult.records;
+                                    if (records == null || records.length < 1)
+                                        return;
+
+                                    var record = records[0];
+                                    if (record == null)
+                                        return;
+
+                                    $scope.buffer.followingCategory[record.categoryId] = true;
+
+
+                                });
+
+                            // Add promise to list.
+                            pPromises.push(pGetCategoryFollowingStatusPromise);
+                        }
+
+                        //#endregion
                     });
 
-                    return Promise.all(getPostCategorizationPromises).then(function(x){
-                        console.log($scope.buffer.categoryPostsCounter);
-                    });
+                    return Promise.all(pPromises)
+                        .then(function (x) {
+                            return getCategoriesResult;
+                        });
+                })
+                .then(function(getCategoriesResult){
+                    $scope.result.getCategories = getCategoriesResult;
                 });
+        };
+
+        /*
+        * Unfollow the specific category.
+        * */
+        $scope.fnUnfollowCategory = function(categoryId){
+            // Category is not being followed yet.
+            if (!$scope.buffer.followingCategory[categoryId])
+                return;
+
+            // Display confirmation message.
+            $ngConfirm({
+                title: ' ',
+                content: '<b class="text-danger">{{"Stop following this category" | translate}}</b>',
+                scope: $scope,
+                buttons: {
+                    ok: {
+                        text: $translate.instant('OK'),
+                        btnClass: 'btn btn-flat btn-danger',
+                        action: function(scope, button){
+                            // Unfollow category.
+                            followCategoryService.unfollowCategory(categoryId)
+                                .then(function(){
+                                    var szMessage = $translate.instant('Stopped following category successfully');
+                                    toastr.success(szMessage);
+
+                                    // Mark the category be unfollowed.
+                                    $scope.buffer.followingCategory[categoryId] = false;
+                                });
+                        }
+                    },
+                    cancel: {
+                        text: $translate.instant('Cancel'),
+                        btnClass: 'btn btn-flat btn-default',
+                        action: function(scope, button){
+                        }
+                    }
+                }
+            });
+        };
+
+        /*
+        * Follow the specific category.
+        * */
+        $scope.fnFollowCategory = function(categoryId){
+            // Category has been followed.
+            if ($scope.buffer.followingCategory[categoryId])
+                return;
+
+            // Display confirmation message.
+            $ngConfirm({
+                title: ' ',
+                content: '<b class="text-info">{{"Start following this category" | translate}}</b>',
+                scope: $scope,
+                buttons: {
+                    ok: {
+                        text: $translate.instant('OK'),
+                        btnClass: 'btn btn-flat btn-primary',
+                        action: function(scope, button){
+                            // Unfollow category.
+                            followCategoryService.followCategory(categoryId)
+                                .then(function(){
+                                    var szMessage = $translate.instant('Followed category successfully');
+                                    toastr.success(szMessage);
+
+                                    // Mark the category be unfollowed.
+                                    $scope.buffer.followingCategory[categoryId] = true;
+                                });
+                        }
+                    },
+                    cancel: {
+                        text: $translate.instant('Cancel'),
+                        btnClass: 'btn btn-flat btn-default',
+                        action: function(scope, button){
+                        }
+                    }
+                }
+            });
         };
 
         /*
         * Called when controller has been initialized successfully.
         * */
-        $timeout(function(){
-            $scope.result.getCategories.records = details.categories;
-            $scope.buffer.categoryPostsCounter = details.categoryPostsCounter;
-            // $scope.result.getCategories = initialGetCategory;
-        });
+        $scope.fnInit = function () {
+            // Search for categories.
+            $scope.condition.getCategories.pagination.page = 1;
+            $scope.fnGetCategories();
+        };
 
         //#endregion
 
