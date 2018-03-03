557254614fe314a73b9a2c1c3795354a2d05d5c4
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-03-03 22:17:23 +0800

    - Added one-signal.service.js

--- A - SOURCE CODE/B - CLIENT/A - WEB/B - USER/src/app/services/one-signal.service.js
+++ A - SOURCE CODE/B - CLIENT/A - WEB/B - USER/src/app/services/one-signal.service.js
@@ -1,3 +1,65 @@
 module.exports = function(ngModule){
+    ngModule.service('oneSignalService', function(){
 
+        //#region Properties
+
+        // Whether service has been initialized or not.
+        this._bHasServiceInitialized = false;
+
+        //#endregion
+
+        //#region Methods
+
+        /*
+        * Add one signal sdk.
+        * */
+        this.addSdk = function(){
+            // Sdk name.
+            var szSdkName = 'onesignal-jssdk';
+
+            // Sdk has been imported before.
+            if (document.getElementById(szSdkName)) {
+                return;
+            }
+
+            var oneSignalSdkScript = document.createElement('script');
+            oneSignalSdkScript.id = szSdkName;
+            oneSignalSdkScript.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js?onload="fnOnOneSignalLoaded"';
+            oneSignalSdkScript.async = true;
+            document.head.appendChild(oneSignalSdkScript);
+        };
+
+        /*
+        * Initialize service with configuration.
+        * */
+        this.init = function(appId, promptOptions, welcomeNotification, notifyButton, autoRegister,persistNotification){
+
+            // Service has been initialized before. Don't initialize it.
+            if (this._bHasServiceInitialized)
+                return;
+
+            // Find one signal instance.
+            var OneSignal = window.OneSignal || [];
+
+            // Options to be initialized.
+            var options = {
+                appId: appId,
+                promptOptions: promptOptions,
+                welcomeNotification: welcomeNotification,
+                notifyButton: notifyButton,
+                autoRegister: autoRegister,
+                persistNotification: persistNotification
+            };
+
+            // Initialize service.
+            OneSignal.push(function() {
+                OneSignal.init(options);
+            });
+
+            // Mark the service as has been initialized.
+            this._bHasServiceInitialized = true;
+        };
+
+        //#endregion
+    });
 };
