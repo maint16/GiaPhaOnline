b5ff73bc13dcf3ba6adafe63e95745952562f4be
ThaiNguyen <nguyendacthai1992@gmail.com>
2018-03-26 11:36:29 +0800

    - Added LifeTimeConstant.cs

---
+++ A - SOURCE CODE/A - SERVICE/Main/Constants/LifeTimeConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Main.Constants
+{
+    public class LifeTimeConstant
+    {
+        /// <summary>
+        /// How many seconds that a jwt exists
+        /// </summary>
+        public const int JwtLifeTime = 18000;
+    }
+}
