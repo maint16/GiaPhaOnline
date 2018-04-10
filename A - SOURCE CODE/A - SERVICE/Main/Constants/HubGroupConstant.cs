be2d6de374b3c8b47e6a88848310dbf66d7c2915
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-04-10 16:59:41 +0800

    - Added user to specific group base on his/her role.

---
+++ A - SOURCE CODE/A - SERVICE/Main/Constants/HubGroupConstant.cs
@@ -0,0 +1,19 @@
+﻿namespace Main.Constants
+{
+    public class HubGroupConstant
+    {
+        #region Properties
+
+        /// <summary>
+        /// Group of administrators.
+        /// </summary>
+        public const string Admin = nameof(Admin);
+
+        /// <summary>
+        /// Group of ordinary users.
+        /// </summary>
+        public const string User = nameof(User);
+
+        #endregion
+    }
+}
