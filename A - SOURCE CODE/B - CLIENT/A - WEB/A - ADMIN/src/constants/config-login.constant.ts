bb76e76b2d1191637c8503ed8e6d01d4db13de9d
dat <datptitcntt@gmail.com>
2018-09-01 22:46:52 +0800

    add login with facebook and google

---
+++ A - SOURCE CODE/B - CLIENT/A - WEB/A - ADMIN/src/constants/config-login.constant.ts
@@ -0,0 +1,7 @@
+export class ConfigLoginConstant {
+  public googleAppId: string = '806371952743-rno7ho38thn702uk77ghq8abj88hc9dp.apps.googleusercontent.com';
+  public facebookAppId : string = '262038564425058';
+  public facebook : string = 'facebook';
+  public google : string = 'google';
+  //#endregion
+}
