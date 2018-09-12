1a5ccc1b1c1d2e58d27e4c1cafe8dd23c545dbfd
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-09-12 11:41:25 +0700

    - Refactored src. - Removed user-detail module, route.

--- A - SOURCE CODE/B - CLIENT/A - WEB/A - ADMIN/src/interfaces/services/user-service.interface.ts
+++ A - SOURCE CODE/B - CLIENT/A - WEB/A - ADMIN/src/interfaces/services/user-service.interface.ts
@@ -2,7 +2,7 @@ import {Observable} from 'rxjs/Observable';
 import {LoadUserViewModel} from '../../view-models/user/load-user.view-model';
 import {User} from '../../models/entities/user';
 import {SearchResult} from '../../models/search-result';
-import {SaveUserStatusViewModel} from '../../view-models/user/save-user-status.view-model';
+import {EditUserStatusViewModel} from '../../view-models/user/edit-user-status.view-model';
 export interface IUserService {
   //#region Methods
   /*
@@ -11,7 +11,12 @@ export interface IUserService {
   getUser();
   // Load users from api end-point.
   loadUsers(conditions: LoadUserViewModel): Observable<SearchResult<User>>;
+
+
   getUserDetail(id: number);
-  saveUserStatus(conditions: SaveUserStatusViewModel);
+
+  // Edit user status using specific condition.
+  editUserStatus(conditions: EditUserStatusViewModel): Observable<any>;
+
   //#endregion
 }
--- A - SOURCE CODE/B - CLIENT/A - WEB/A - ADMIN/src/modules/user/user-detail/user-detail.component.ts
+++ A - SOURCE CODE/B - CLIENT/A - WEB/A - ADMIN/src/modules/user/user-detail/user-detail.component.ts
@@ -1,9 +1,11 @@
 import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
-import {UserViewModel} from '../../../view-models/user.view-model';
 import {IUserService} from '../../../interfaces/services/user-service.interface';
 import {ToastrService} from 'ngx-toastr';
-import {SaveUserStatusViewModel} from '../../../view-models/user/save-user-status.view-model';
+import {EditUserStatusViewModel} from '../../../view-models/user/edit-user-status.view-model';
 import {TranslateService} from '@ngx-translate/core';
+import {User} from '../../../models/entities/user';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+
 @Component({
   selector: 'user-detail',
   templateUrl: 'user-detail.component.html',
@@ -11,46 +13,85 @@ import {TranslateService} from '@ngx-translate/core';
 })
 
 export class UserDetailComponent implements OnInit {
-  public user: UserViewModel;
+
+  //#region Properties
+
+  // User instance to display on modal dialog.
+  @Input('user')
+  public user: User;
+
   @Output() closeModal = new EventEmitter<boolean>();
   public statuss = [
     {category: 'Active', value: 1},
     {category: 'Inactive', value: 2}
-    ];
+  ];
+
   public status = {};
-  _userId: number;
-  get userId(): number {
-    return this._userId;
-  }
-  @Input('userId')
-  set userId(value: number) {
-  this._userId = value;
-  if (this._userId != null) {
-  this.userService.getUserDetail(this._userId).subscribe((data: any) => {
-  this.user = new UserViewModel();
-  this.user.email = data.email;
-  this.user.nickname = data.nickname;
-  this.user.status = data.status;
-});
-}
-}
-  public constructor(@Inject('IUserService') private userService: IUserService, private toastr: ToastrService,
-                     private translate: TranslateService) {
+  // _userId: number;
+  // get userId(): number {
+  //   return this._userId;
+  // }
+  //
+  // @Input('userId')
+  // set userId(value: number) {
+  //   this._userId = value;
+  //   if (this._userId != null) {
+  //     this.userService.getUserDetail(this._userId).subscribe((data: any) => {
+  //       this.user = new User();
+  //       this.user.email = data.email;
+  //       this.user.nickname = data.nickname;
+  //       this.user.status = data.status;
+  //     });
+  //   }
+  // }
+
+  //#endregion
+
+
+  //#region Constructor
+
+  // Initialize component with injectors.
+  public constructor(@Inject('IUserService') private userService: IUserService,
+                     private toastr: ToastrService,
+                     private translate: TranslateService,
+                     public bsModalRef: BsModalRef) {
     translate.setDefaultLang('en');
   }
 
-  ngOnInit() {
+  //#endregion
+
+  //#region Methods
+
+  // Called when component is initialized.
+  public ngOnInit(): void {
+    if (!this.user)
+      this.user = new User();
   }
-  public saveUserStatus() {
-    let saveUserStatusViewModel = new SaveUserStatusViewModel();
-    saveUserStatusViewModel.userId = this.user.id;
-    saveUserStatusViewModel.userStatus = this.user.status;
-  this.userService.saveUserStatus(saveUserStatusViewModel).subscribe((data: any) => {
-    this.toastr.success('Update Successfully');
-    this.closeModal.emit(true);
-  });
+
+  // Called when OK button is clicked.
+  public ngOnOkClicked(): void {
+    // User is defined before being passed to component, this means user information should be updated.
+    if (!this.user || !this.user.id)
+      return;
+
+    let user = this.user;
+
+    let info = new EditUserStatusViewModel();
+    info.userId = user.id;
+    info.userStatus = user.status;
+
+    this.userService
+      .editUserStatus(info)
+      .subscribe(() => {
+        // Close modal dialog.
+        this.bsModalRef.hide();
+      });
   }
-  public cancel() {
-    this.closeModal.emit(true);
+
+  // Called when cancel button is clicked on modal.
+  public ngOnCancelClicked(): void {
+    this.bsModalRef.hide();
   }
+
+  //#endregion
 }
